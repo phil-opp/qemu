03772d6c44bf3661cba61fe8eb3c3990bcb821b9
Philipp Oppermann <dev@phil-opp.com>
2018-11-29 19:58:21 +0100

    Try to corrupt only kernel heap

--- scripts/qmp/redox.py
+++ scripts/qmp/redox.py
@@ -11,15 +11,16 @@ def main():
     global inj
     inj = fault_injection.FaultInjectionFramework(sys.argv[1], 0)
 
-    inj.notify(1 * 1000 * 1000 * 1000, corrupt_random_bit)
+    inj.notify(1 * 1000 * 1000 * 1000, corrupt_random_heap_bit)
 
     inj.run()
 
     sys.exit(1)
 
-def corrupt_random_bit():
-    #print 'corrupt_random_bit()'
-    byte_number = random.randint(0, 2048 * 1024 * 1024)
+def corrupt_random_heap_bit():
+    #print 'corrupt_random_heap_bit()'
+    virt_byte_number = 0xfffffe8000000000 + random.randint(0, 0x100000);
+    byte_number = virt_to_phys(virt_byte_number);
     bit_number = random.randint(0, 7);
     byte = inj.read(byte_number, 1, 0)["value"]
 
@@ -34,7 +35,33 @@ def corrupt_random_bit():
     #print ("corrupted value: " + hex(byte))
     inj.write(byte_number, byte, 1, 0)
 
-    inj.notify(1 * 1000 * 1000 * 1000, corrupt_random_bit) # every second
+    inj.notify(1 * 1000 * 1000 * 1000, corrupt_random_heap_bit) # every second
+
+def virt_to_phys(addr):
+    # Translate a virtual to a physical address
+    p4_index = (addr >> 12 >> 9 >> 9 >> 9) & 0x1ff;
+    p3_index = (addr >> 12 >> 9 >> 9) & 0x1ff;
+    p2_index = (addr >> 12 >> 9) & 0x1ff;
+    p1_index = (addr >> 12) & 0x1ff;
+    page_offset = addr & 0xfff;
+
+    p4_addr = inj.read_cr3(0)["value"];
+
+    p4_entry = inj.read(p4_addr + p4_index * 8, 8, 0)["value"]
+    p3_addr = p4_entry & (~0xfff)
+
+    p3_entry = inj.read(p3_addr + p3_index * 8, 8, 0)["value"]
+    p2_addr = p3_entry & (~0xfff)
+
+    p2_entry = inj.read(p2_addr + p2_index * 8, 8, 0)["value"]
+    p1_addr = p2_entry & (~0xfff)
+
+    p1_entry = inj.read(p1_addr + p1_index * 8, 8, 0)["value"]
+    phys_page_addr = p1_entry & (~0xfff)
+
+    phys_addr = phys_page_addr + page_offset
+
+    return phys_addr
 
 if __name__ == '__main__':
     main()
